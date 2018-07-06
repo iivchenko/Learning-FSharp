5986698b622e2b821633dc03ba66f771dcaa39a6
Ivan Ivchenko <ivan.ivchenko@betssongroup.com>
2018-07-06 18:41:47 +0300

    coding

---
+++ FSharp.Tests/LinqTestsImperative.fs
@@ -0,0 +1,10 @@
+﻿namespace FSharp.Tests
+
+open NUnit.Framework
+
+[<TestFixture>]
+type LinqTestsImperative() = 
+    
+    (* TODO: Study imperative programming and write LINQ tests in inerative way*)
+    member public this.emptyMethod()=
+        printfn "Hello"
---
+++ FSharp.Tests/Math.fs
@@ -0,0 +1,25 @@
+﻿namespace FSharp.Tests
+
+open NUnit.Framework
+
+[<TestFixture>]
+type Math() = 
+    
+    (* Factorial function *)
+    let rec (!) x =
+        match x with
+        | _ when x <= 1 -> 1
+        | _             -> x + ! x - 1
+
+    let (!=) value1 value2 =
+        value1 <> value2
+
+    [<Test>]
+    member public this.``Factorial (!) test``()=
+        printf "%d" !5
+
+    [<Test>]
+    member public this.``Not equals (!=) operator test``()=
+        printfn "%b" (2 != 1)
+        printfn "%b" (1 != 2)
+        printfn "%b" (2 != 2)
