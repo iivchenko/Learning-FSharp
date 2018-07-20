80dabd1b55c4f122839e241ef8716c869db13217
Ivan Ivchenko <ivan.ivchenko@betssongroup.com>
2018-07-20 17:46:22 +0300

    Provide some unit tests for MyList

--- FSharp.Tests/DataStructures/MyList.fs
+++ FSharp.Tests/DataStructures/MyList.fs
@@ -1,10 +1,24 @@
 module public MyList
 
+(* Write unit tests instead of main method!!*)
+
 type List<'T> =
     | Node of 'T * List<'T>
     | Empty    
-    static member (+) (x, y) =
-        Node(x, y)
+    static member (+) (x, tail) =
+        Node(x, tail)
+    static member (+) (tail, x) =
+        let rec appendInternal list acc = 
+            match list with 
+            | Empty -> Node(acc, Empty)
+            | Node(x, tail) -> Node(x, (appendInternal tail acc))
+        appendInternal tail x
+    static member (+) (head, tail) =
+        let rec concatInternal list acc = 
+            match list with 
+            | Empty -> acc
+            | Node(x, tail) -> Node(x, (concatInternal tail acc))        
+        concatInternal head tail
 
 let rec iter action list  =
     match list with
@@ -16,14 +30,14 @@ let rec iter action list  =
 let rec filter predicate list =
     match list with
     | Empty -> Empty
-    | Node(head, tail) when predicate head ->  head + filter predicate tail
+    | Node(head, tail) when predicate head -> (Node(head, filter predicate tail)) 
     | Node(_, tail) -> filter predicate tail
 
 let rec map transform list =
     match list with
     | Empty -> Empty
     | Node(head, tail) ->
-        transform head + map transform tail
+        Node(transform head, map transform tail)
 
 let rec first predicate list =
     match list with
@@ -39,39 +53,78 @@ let last predicate list =
         | Node(_, tail) -> realyLast predicate tail acc
     realyLast predicate list Unchecked.defaultof<'T>
 
+let rec all predicate list =
+    match list with 
+    | Empty -> true
+    | Node(x, tail) when predicate x ->
+        all predicate tail
+    | Node(_, _) -> false
+
+let rec any predicate list = 
+    match list with 
+    | Empty -> false
+    | Node(x, _) when predicate x -> true
+    | Node(_, tail) ->
+        any predicate tail
+
 let rec max compare list =
     match list with
     | Empty -> Unchecked.defaultof<'T>
     | Node(x, Empty) -> x
-    | Node(x, Node(y, tail)) when compare x y = 1 -> max compare <| x + tail
-    | Node(x, Node(y, tail)) -> max compare <| y + tail
+    | Node(x, Node(y, tail)) when compare x y = 1 -> max compare (Node(x, tail))
+    | Node(_, Node(y, tail)) -> max compare <| (Node(y, tail))
 
 let rec min compare list =
     match list with
     | Empty -> Unchecked.defaultof<'T>
     | Node(x, Empty) -> x
-    | Node(x, Node(y, tail)) when compare x y = -1 -> min compare <| x + tail
-    | Node(x, Node(y, tail)) -> min compare <| y + tail
+    | Node(x, Node(y, tail)) when compare x y = -1 -> min compare (Node(x, tail))
+    | Node(_, Node(y, tail)) -> min compare <| (Node(y, tail))
 
 let reverse list =
     let rec realyReverse list2 acc =
         match list2 with
         | Empty -> acc
-        | Node(x, tail) -> realyReverse tail <| x + acc
+        | Node(x, tail) -> realyReverse tail (Node(x, acc))
     realyReverse list Empty
 
+(* TODO *)
+(* Select Many*)
+(* Aggregate*)
+(* Average *)
+(* Append *)
+(* Concat *)
+
+let count predicate list =
+    let rec countInternal predicate list acc =
+        match list with
+        | Empty -> acc
+        | Node(x, tail) when predicate x ->
+            countInternal predicate tail acc + 1
+        | Node(_, tail) ->
+            countInternal predicate tail acc
+    countInternal predicate list 0
+
+let rec contains compare list =
+    match list with
+    | Empty -> false
+    | Node(x, _) when compare x ->
+        true
+    | Node(_, tail) ->
+        contains compare tail
+
+(* Sort algorithms *)
 let rec selectionSort compare list =
     (* Finish him!!*)
     match list with
     | Empty -> Empty
-    | Node(x, Empty) -> list
+    | Node(_, Empty) -> list
     | Node(x, Node(y, tail)) when compare x y = 1 -> 
         Node(x, selectionSort compare <| Node(y, tail))
     | Node(x, Node(y, tail)) -> 
         selectionSort compare <| Node(y, selectionSort compare <| Node(x, tail))
 
-(* Select Many*)
-
+(* List operators *)   
 let inline (!-) x =
     fun next -> Node(x, next)
 
@@ -81,46 +134,62 @@ let inline (-!-) x y =
 let inline (-!) x y =
     x <| Node(y, Empty)
 
-[<EntryPoint>]
-let main args =
+//[<EntryPoint>]
+//let main args =
 
-    let list1 = !- 1 -!- 2 -!- 3 -!- 7 -!- 4 -! 5   
+//    let list1 = !- 1 -!- 2 -!- 3 -!- 7 -!- 4 -! 5   
     
-    //iter (fun x -> (printfn "%i" x)) list1
+//    //iter (fun x -> (printfn "%i" x)) list1
 
-    let list2 = 1 + list1
+//    let list2 = 1 + list1
     
-    printf "Initial "
-    iter (fun x -> (printf "%i " x)) list2
+//    printf "Initial "
+//    iter (fun x -> (printf "%i " x)) list2
 
-    printfn ""
+//    printfn ""
 
-    printf "Map and Filtered "
+//    printf "Map and Filtered "
 
-    let list3 =
-        list2 
-        |> map (fun x -> x + 1)
-        |> filter (fun x -> x % 2 = 0)
+//    let list3 =
+//        list2 
+//        |> map (fun x -> x + 1)
+//        |> filter (fun x -> x % 2 = 0)
     
-    iter (fun x -> (printf "%i " x)) list3
+//    iter (fun x -> (printf "%i " x)) list3
     
-    printfn ""
+//    printfn ""
 
-    printf "Reverse "
-    list3
-    |> reverse
-    |> iter (fun x -> (printf "%i " x))
+//    printf "Reverse "
+//    list3
+//    |> reverse
+//    |> iter (fun x -> (printf "%i " x))
 
-    printfn ""
+//    printfn ""
 
-    printfn "Sorted is %i" <| selectionSort (fun x y -> if x > y then 1 else -1) list3
+//    //printfn "Sorted is %i" <| selectionSort (fun x y -> if x > y then 1 else -1) list3
 
-    printfn ""
+//    printfn ""
     
-    printfn "First is %i" <| first (fun x -> x % 4 = 0) list3
-    printfn "Last is %i" <| last (fun x -> x % 4 = 0) list3
-    printfn "Max is %i" <| max (fun x y -> if x > y then 1 else -1) list3
-    printfn "Min is %i" <| min (fun x y -> if x > y then 1 else -1) list3
+//    printfn "First is %i" <| first (fun x -> x % 4 = 0) list3
+//    printfn "Last is %i" <| last (fun x -> x % 4 = 0) list3
+//    printfn "Max is %i" <| max (fun x y -> if x > y then 1 else -1) list3
+//    printfn "Min is %i" <| min (fun x y -> if x > y then 1 else -1) list3
+//    printfn "Contains (4) %b" <| contains (fun x -> x = 4) list3
+//    printfn "Contains (3) %b" <| contains (fun x -> x = 3) list3
+//    printfn "Count = %i" <| count (fun x -> true) list3
+//    printfn "Count x\4 = %i" <| count (fun x -> x % 4 = 0) list3
+//    printfn "All x\2 = %b" <| all (fun x -> x % 2 = 0) list3
+//    printfn "All x\4 = %b" <| all (fun x -> x % 4 = 0) list3
+//    printfn "Any x\4 = %b" <| any (fun x -> x % 4 = 0) list3
+//    printfn "Any x\5 = %b" <| all (fun x -> x % 5 = 0) list3
     
-    
-    0
+//    let list4 = list3 + 15
+//    printf "List4: " 
+//    iter (fun x -> (printf "%i " x)) list4
+
+//    let list5 = list1 + list3
+//    printfn "List5: " 
+//    iter (fun x -> (printf "%i " x)) list5
+
+
+//    0
---
+++ FSharp.Tests/DataStructures/MyListTests.fs
@@ -0,0 +1,81 @@
+﻿namespace FSharp.Tests.DataStructures
+
+open NUnit.Framework
+open MyList
+
+[<TestFixture>]
+type MyListTests() = 
+
+    let rec assertEquals (expected:MyList.List<'T>) (actual:MyList.List<'T>) =
+        match expected, actual with 
+        | Empty, Empty -> ()
+        | Node(x, tailExpected), Node(y, tailActual) ->
+            assertEquals tailExpected tailActual
+            Assert.That(y, Is.EqualTo(x))
+        | _, _ -> failwith "Different lists size!"
+
+    [<Test>]
+    member this.`` 'filter' test ``() =
+
+        let initial = MyList.Node(1, MyList.Node(2, MyList.Node(3, MyList.Node(4, MyList.Empty))))
+        let expected = MyList.Node(2, MyList.Node(4, MyList.Empty))
+        let actual = MyList.filter (fun x -> x % 2 = 0) initial
+
+        printf "Initial list: "
+        MyList.iter (fun x -> (printf "%i " x)) initial
+
+        printfn ""
+
+        printf "Expected list: "
+        MyList.iter (fun x -> (printf "%i " x)) expected
+
+        printfn ""
+
+        printf "Actual list: "
+        MyList.iter (fun x -> (printf "%i " x)) actual       
+
+        assertEquals expected actual
+
+    [<Test>]
+    member this.`` 'map' test ``() =
+
+        let initial = MyList.Node(1, MyList.Node(2, MyList.Node(3, MyList.Node(4, MyList.Empty))))
+        let expected = MyList.Node(2, MyList.Node(3, MyList.Node(4, MyList.Node(5, MyList.Empty))))
+        let actual = MyList.map (fun x -> x + 1) initial
+
+        printf "Initial list: "
+        MyList.iter (fun x -> (printf "%i " x)) initial
+
+        printfn ""
+
+        printf "Expected list: "
+        MyList.iter (fun x -> (printf "%i " x)) expected
+
+        printfn ""
+
+        printf "Actual list: "
+        MyList.iter (fun x -> (printf "%i " x)) actual
+
+        assertEquals expected actual
+
+    [<Test>]
+    member this.`` 'first' test; If list is EMPTY then return default value``() =
+
+        let initial = MyList.Empty
+        let expected = 0
+        let actual = MyList.first (fun x -> x > 0) initial
+
+        printf "Initial list: "
+        MyList.iter (fun x -> (printf "%i " x)) initial
+
+        printfn ""
+
+        printf "Expected value : "
+        printf "%i" expected
+
+        printfn ""
+
+        printf "Actual value : "
+        printf "%i" actual
+        
+        Assert.That(actual, Is.EqualTo(expected))
