e73aa63c9c76b1a31c7bee29292e49f37287130e
Ivan Ivchenko <ivan.ivchenko@betssongroup.com>
2018-07-27 20:15:27 +0300

    Add simple game 'Guess number'. Provide basic console module

---
+++ LearningFSharp.Games.GuessNumber/ConsoleModule.fs
@@ -0,0 +1,58 @@
+﻿namespace LearningFSharp.Games.GuessNumber
+
+open System
+
+module ConsoleModule =
+
+    let cls ()=
+        Console.Clear()
+    
+    let inline readLn () =
+        Console.ReadLine()
+
+    type Text =
+    // think about STRING, Upper, lowwer text etc.
+    | Gray of string
+    | Green of string 
+    | Yellow of string 
+    | Red of string 
+    | Line
+    | Text of Text * Text
+        static member (+) (left, right) =
+            Text(left, right)
+
+    let rec write (text:Text) =
+        let origin = Console.ForegroundColor
+        
+        match text with
+        | Gray(str) -> 
+            Console.ForegroundColor <- ConsoleColor.Gray
+            Console.Write(str)
+        | Green(str) -> 
+            Console.ForegroundColor <- ConsoleColor.Green
+            Console.Write(str)
+        | Yellow(str) -> 
+            Console.ForegroundColor <- ConsoleColor.Yellow
+            Console.Write(str)
+        | Red(str) -> 
+            Console.ForegroundColor <- ConsoleColor.Red
+            Console.Write(str)
+        | Line -> 
+            Console.WriteLine()
+        | Text(part1, part2) -> 
+            write part1
+            write part2
+
+        Console.ForegroundColor <- origin
+
+    let green text =
+        Green(text)
+    
+    let yellow text = 
+        Yellow(text)
+
+    let gray text = 
+        Gray(text)
+
+    let red text = 
+        Red(text)
---
+++ LearningFSharp.Games.GuessNumber/Program.fs
@@ -0,0 +1,49 @@
+﻿open System
+open LearningFSharp.Games.GuessNumber.ConsoleModule
+
+let rec gameLoop (random:Random) =
+    let rec internalGameLoop number =
+        let (|Int|_|) (str : string) =
+           match Int32.TryParse(str) with
+            | (true,int) -> Some(int)
+            | _ -> None
+
+        let rec getGuess () =
+            let answer = readLn ()
+            match answer with
+            | Int i -> i
+            | _ -> 
+                write <| red "It is not a number!! Try again : "
+                getGuess ()   
+
+        let guess = getGuess ()
+
+        match guess with
+        | _ when guess = number -> 
+            write <| green "Perfect!" + Line
+        | _ when guess > number ->
+            write <| gray "It should be " + yellow "less" + gray ". Try once more : "
+            internalGameLoop number
+        | _  -> 
+            write <| gray "It should be " + yellow "more" + gray ". Try once more : "
+            internalGameLoop number
+
+    let number = random.Next(1, 10)
+    write <| gray "Guess number: "
+
+    internalGameLoop number
+
+    write <| gray "Want to start again? (y/n) : "
+    
+    match readLn () with 
+    | "y" | "Y" | "yes" | "YES" -> 
+        cls()
+        gameLoop random
+    | _ -> ()
+
+[<EntryPoint>]
+let main argv =
+    
+    gameLoop <| new Random()
+
+    0
