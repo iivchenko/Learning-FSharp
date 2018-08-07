c7e312990be0628c6cfcca495bc07d9ffaba71b4
Ivan Ivchenko <ivan.ivchenko@betssongroup.com>
2018-08-07 17:53:22 +0300

    Improve 'Snake'. Make code much more elegant. Disable snake to move back

--- LearningFSharp.Games.Snake/Program.fs
+++ LearningFSharp.Games.Snake/Program.fs
@@ -1,6 +1,4 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
+﻿open System
 open System.Threading
 
 type Position =
@@ -16,6 +14,18 @@ type Direction =
     | Right
     | None
 
+let (| Direction |) (direction:Direction) (key:System.ConsoleKey) =
+    match key with
+    | ConsoleKey.UpArrow when direction = Down -> None
+    | ConsoleKey.UpArrow -> Up
+    | ConsoleKey.DownArrow when direction = Up  -> None
+    | ConsoleKey.DownArrow -> Down
+    | ConsoleKey.LeftArrow when direction = Right  -> None
+    | ConsoleKey.LeftArrow -> Left
+    | ConsoleKey.RightArrow when direction = Left  -> None
+    | ConsoleKey.RightArrow -> Right
+    | _ -> None
+
 type Snake =
     | Head of Position * Direction * Snake
     | Body of Position * Snake
@@ -25,86 +35,85 @@ let draw (x, y) (symbol:char) =
     Console.SetCursorPosition(x, y)
     Console.Write(symbol)
 
-let rec drawSnake snake drawHead drawBody =
+let rec processSnake processHead processBody processTail snake =
     match snake with
     | Head({ X = x; Y = y }, direction, body) ->
-        drawHead x y
-        drawSnake body drawHead drawBody
-    | Body({ X = x; Y = y }, body) -> 
-        drawBody x y
-        drawSnake body drawHead drawBody
-    | Tail -> ()
+        processHead x y
+        Head({ X = x; Y = y }, direction, (processSnake processHead processBody processTail body))
+    | Body({ X = x; Y = y }, body) ->
+        processBody x y
+        Body({ X = x; Y = y }, (processSnake processHead processBody processTail body))
+    | Tail -> 
+        processTail
+        Tail
 
-let canUpdateDirection direction snake =
-    match snake with
-    | Head({X = px; Y = py}, _, Body({X = bx; Y = by},_)) ->
-        match direction with 
-        | Up -> 
-            match py - 1 = by with
-            | true -> false
-            | false -> true
-        | Down -> 
-            match py + 1 = by with
-            | true -> false
-            | false -> true
-        | Left -> 
-            match px - 1 = bx with
-            | true -> false
-            | false -> true
-        | Right -> 
-            match px + 1 = bx with
-            | true -> false
-            | false -> true
-        | None -> false
-    | _ -> true
+let clearSnake snake =
+    processSnake (fun x y -> draw (x, y) ' ') (fun x y -> draw (x, y) ' ') () snake
 
-let updateDirection direction snake =
-    match snake with
-    | Head(postion, _, tail) ->
-        Head(postion, direction, tail)
+let drawSnake snake = 
+    processSnake (fun x y -> draw (x, y) '0') (fun x y -> draw (x, y) '*') () snake
 
-// TODO: Apply Direction
-let rec moveSnake (x, y) snake =
+let updateSnakeDirection snake =
     match snake with
-    | Head({ X = px; Y = py }, direction, body) ->
-        Head({X = px + x; Y = py + y }, direction, (moveSnake (px, py) body))
-    | Body({ X = px; Y = py }, body) ->
-        Body({X = x; Y = y }, (moveSnake (px, py) body))
-    | Tail -> Tail
+    | Head(position, direction, body) ->
+        match Console.ReadKey().Key with 
+        | Direction direction Up -> 
+            Head(position, Up, body)
+        | Direction direction Down  ->
+            Head(position, Down, body)
+        | Direction direction Left ->
+            Head(position, Left, body)
+        | Direction direction Right ->
+            Head(position, Right, body)
+        | _ -> Head(position, direction, body)    
+    | Body(_, _)
+    | Tail ->
+        failwith "Snake should have Head!!"
+
+let moveSnake snake =
+    let rec moveInternal (x, y) snake =    
+        match snake with
+        | Head({ X = px; Y = py }, direction, body) ->
+            Head({X = px + x; Y = py + y }, direction, (moveInternal (px, py) body))
+        | Body({ X = px; Y = py }, body) ->
+            Body({X = x; Y = y }, (moveInternal (px, py) body))
+        | Tail -> Tail
+    match snake with
+    | Head(_, Up, _) ->
+        moveInternal (0, -1) snake
+    | Head(_, Down, _) ->
+        moveInternal (0, 1) snake
+    | Head(_, Left, _) ->
+        moveInternal (-1, 0) snake
+    | Head(_, Right, _) ->
+        moveInternal (1, 0) snake
+    | Head(_, None, _) -> failwith "Snake should have a Direction!!"
+    | Body(_, _)
+    | Tail ->
+        failwith "Snake should have Head!!"
 
 let rec gameLoop snake speed =
+    
     let current = DateTime.Now
 
-    let direction = 
-        match Console.ReadKey().Key with
-        | ConsoleKey.UpArrow -> Up
-        | ConsoleKey.DownArrow -> Down
-        | ConsoleKey.LeftArrow -> Left
-        | ConsoleKey.RightArrow -> Right
-        | _ -> None
-
-    drawSnake snake (fun x y -> draw (x, y) ' ') (fun x y -> draw (x, y) ' ')
-
-    match canUpdateDirection direction snake with
-    | true -> snake = updateDirection direction snake     
-
-    let snake = if canUpdateDirection direction snake then updateDirection direction snake else snake
-    let snake = snake |> moveSnake
-
-    drawSnake newSnake (fun x y -> draw (x, y) '0') (fun x y -> draw (x, y) '*')
+    let snake = 
+        snake 
+            |> clearSnake
+            |> updateSnakeDirection
+            |> moveSnake
+            |> drawSnake
     
     match DateTime.Now.Subtract(current) with
     | elapsed when elapsed + speed > TimeSpan.Zero -> Thread.Sleep(elapsed)
     | _ -> ()
 
-    gameLoop newSnake speed
+    gameLoop snake speed
 
 [<EntryPoint>]
 let main argv =
     Console.CursorVisible <- false
 
     // TODO:
-    // Avoid Snake moving back
     // Implement async control input
     // Game over if snake it self
     // Game over if snake move away of the screen
