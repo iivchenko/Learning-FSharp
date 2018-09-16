cb7d2a37cdcacd12be4787f9acbe89efe45bbd71
Ivan Ivchenko <ivan.ivchenko@betssongroup.com>
2018-09-16 11:00:05 +0300

    sda

---
+++ LearningFSharp.Bytecode.SimpleAssembler/CommandModule.fs
@@ -0,0 +1,7 @@
+﻿module LearningFSharp.Bytecode.SimpleAssembler.CommandModule
+    open System
+
+    type System.String with 
+        member this.IsCommand (command : string) = 
+            this.StartsWith(command, StringComparison.OrdinalIgnoreCase)
+
