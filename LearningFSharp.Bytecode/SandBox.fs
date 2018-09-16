07d8c804007873acfdc3bd668b1f4cd3638f37ad
Ivan Ivchenko <ivan.ivchenko@betssongroup.com>
2018-09-16 08:45:15 +0300

    Bytecode draft

---
+++ LearningFSharp.Bytecode/SandBox.fs
@@ -0,0 +1,21 @@
+﻿namespace LearningFSharp.Bytecode
+
+    type IStack =
+        abstract Push : int -> unit
+        abstract Pop : unit -> int
+
+    type ICommand =
+        abstract Execute : unit -> unit
+
+    type ICommandIdentifier =
+        abstract Is : string -> bool
+
+    type ICommandValidator =
+        abstract Validate : unit -> unit    
+
+    type IBytecode =
+        abstract Execute : string -> unit
+
+    type IBytecodeBuilder =
+        abstract Add : ICommand -> IBytecodeBuilder
+        abstract Build : unit -> IBytecode
