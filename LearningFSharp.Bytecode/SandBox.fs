2b988acc827cfb56e92526c7771f21197dd7d227
Ivan Ivchenko <ivan.ivchenko@betssongroup.com>
2018-09-16 09:20:55 +0300

    Bytecode. Implement Stack

--- LearningFSharp.Bytecode/SandBox.fs
+++ LearningFSharp.Bytecode/SandBox.fs
@@ -4,6 +4,22 @@
         abstract Push : int -> unit
         abstract Pop : unit -> int
 
+    type Stack =
+        val mutable _stack : int list
+
+        new() = { _stack = [] } 
+
+        interface IStack with 
+            member this.Push value =
+                this._stack <- value::this._stack
+
+            member this.Pop() =
+                match this._stack with
+                | [] -> failwith "Stack is empty!"
+                | head::tail -> 
+                    this._stack <- tail
+                    head
+
     type ICommand =
         abstract Execute : unit -> unit
 
